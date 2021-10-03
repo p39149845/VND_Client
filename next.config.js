7f374a93eb5f610d14a5313adab241fea8986404
frosena <zeroigt@gmail.com>
2021-10-03 14:36:58 +0700

    make it better

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   reactStrictMode: true,
-  webpack5: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+        // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+        config.resolve.fallback = {
+            fs: false
+        }
+    }
+
+    return config;
+}
 }
 
